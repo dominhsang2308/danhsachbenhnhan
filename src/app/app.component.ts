aaccba199d6bc064605c84dfa492764934fe8fa3
dominhsang2308 <76006254+dominhsang2308@users.noreply.github.com>
2022-05-06 16:54:58 +0700

    daxongcachdanhsachbenhnhan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,64 +9,76 @@ import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Crud';
-  displayedColumns: string[] = ['tenbenhnhan', 'hokhau', 'gioitinh', 'ngaysinh', 'phongbenh', 'benh', 'action'];
+  displayedColumns: string[] = [
+    'tenbenhnhan',
+    'hokhau',
+    'gioitinh',
+    'ngaysinh',
+    'phongbenh',
+    'benh',
+    'action',
+  ];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(public dialog: MatDialog, private api: ApiService) {
-
-  }
+  constructor(public dialog: MatDialog, private api: ApiService) {}
   ngOnInit(): void {
-    this.getallbenhnhanList()
+    this.getallbenhnhanList();
   }
+
   openDialog() {
-    this.dialog.open(DialogComponent, {
-      width: '30%',
-    }).afterClosed().subscribe(val => {
-      if (val === 'save') {
-        this.getallbenhnhanList()
-      }
-    })
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+       this.getallbenhnhanList()
+      });
   }
+  //Hiển thị data lên table
   getallbenhnhanList() {
     this.api.getdanhsachbenhnhanApi().subscribe({
       next: (res) => {
         this.dataSource = new MatTableDataSource(res);
         this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort
+        this.dataSource.sort = this.sort;
       },
       error: (error) => {
-        console.log(error)
-      }
-    })
+        console.log(error);
+      },
+    });
   }
+  
   editBenhNhan(row: any) {
-    this.dialog.open(DialogComponent, {
-      width: '30%',
-      data: row
-    }).afterClosed().subscribe(val => {
-      if (val === 'update') {
-        this.getallbenhnhanList()
-      }
-    })
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+        data: row,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getallbenhnhanList();
+        }
+      });
   }
   deletebenhnhan(id: number) {
-    this.api.deletedanhsachbenhnhanApi(id).
-      subscribe({
-        next: (res) => {
-          alert('Xóa thành công')
-          this.getallbenhnhanList()
-        },
-        error: () => {
-          alert('Xóa thất bại')
-        }
-      })
+    this.api.deletedanhsachbenhnhanApi(id).subscribe({
+      next: (res) => {
+        alert('Xóa thành công');
+        this.getallbenhnhanList();
+      },
+      error: () => {
+        alert('Xóa thất bại');
+      },
+    });
   }
   //Filter data
   applyFilter(event: Event) {
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,67 +1,99 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { ApiService } from '../services/api.service';
-import {MatDialogRef,MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent implements OnInit {
   loaiphongbenh = ['Nội trú', ' Ngoại trú'];
-  thembenhnhanform!: FormGroup
-  actionBtn:string = 'Save'
-  constructor(private benhnhanform: FormBuilder,
-    @Inject (MAT_DIALOG_DATA) public editdata: any,
-     private api: ApiService,
-      private dialogRef : MatDialogRef<DialogComponent>) { }
-
+  // thembenhnhanform!: FormGroup
+  actionBtn: string = 'Save';
+  titleaction: string = 'Thêm bệnh nhân';
+  constructor(
+    private benhnhanform: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public editdata: any,
+    private api: ApiService,
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) {}
+  thembenhnhanform = new FormGroup({
+    tenbenhnhan: new FormControl('',[Validators.required]),
+    hokhau: new FormControl('',[Validators.required]),
+    gioitinh: new FormControl('',[Validators.required]),
+    ngaysinh: new FormControl('',[Validators.required]),
+    phongbenh: new FormControl('',[Validators.required]),
+    benh: new FormControl('',[Validators.required]),
+  });
   ngOnInit(): void {
-    this.thembenhnhanform = this.benhnhanform.group({
-      tenbenhnhan: ['', Validators.required],
-      hokhau: ['', Validators.required],
-      gioitinh: ['', Validators.required],
-      ngaysinh: ['', Validators.required],
-      phongbenh: ['', Validators.required],
-      benh:['', Validators.required]
-    })
-    if(this.editdata){
-      this.actionBtn = 'Update'
-      this.thembenhnhanform.controls['tenbenhnhan'].setValue(this.editdata.tenbenhnhan)
-      this.thembenhnhanform.controls['hokhau'].setValue(this.editdata.hokhau)
-      this.thembenhnhanform.controls['gioitinh'].setValue(this.editdata.gioitinh)
-      this.thembenhnhanform.controls['ngaysinh'].setValue(this.editdata.ngaysinh)
-      this.thembenhnhanform.controls['phongbenh'].setValue(this.editdata.phongbenh)
-      this.thembenhnhanform.controls['benh'].setValue(this.editdata.benh)
+    // this.thembenhnhanform = this.benhnhanform.group({
+    //   tenbenhnhan: ['', Validators.required],
+    //   hokhau: ['', Validators.required],
+    //   gioitinh: ['', Validators.required],
+    //   ngaysinh: ['', Validators.required],
+    //   phongbenh: ['', Validators.required],
+    //   benh:['', Validators.required]
+    // })
+   
+    if (this.editdata) {
+      this.actionBtn = 'Update';
+      this.titleaction = 'Sửa bệnh nhân';
+      this.thembenhnhanform.controls['tenbenhnhan'].setValue(
+        this.editdata.tenbenhnhan
+      );
+      this.thembenhnhanform.controls['hokhau'].setValue(this.editdata.hokhau);
+      this.thembenhnhanform.controls['gioitinh'].setValue(
+        this.editdata.gioitinh
+      );
+      this.thembenhnhanform.controls['ngaysinh'].setValue(
+        this.editdata.ngaysinh
+      );
+      this.thembenhnhanform.controls['phongbenh'].setValue(
+        this.editdata.phongbenh
+      );
+      this.thembenhnhanform.controls['benh'].setValue(this.editdata.benh);
     }
   }
+  //Thêm bệnh nhân
   addBenhnhan() {
-   if(!this.editdata){
-    if (this.thembenhnhanform.valid) {
-      this.api.danhsachbenhnhanApi(this.thembenhnhanform.value).
-        subscribe({
+    if (!this.editdata) {
+      if (this.thembenhnhanform.valid) {
+        this.api.danhsachbenhnhanApi(this.thembenhnhanform.value).subscribe({
           next: (res) => {
-            alert('Thêm thành công')
+            alert('Thêm thành công');
             this.thembenhnhanform.reset();
-            this.dialogRef.close()
+            this.dialogRef.close();
           },
           error: () => {
-            alert('Thêm thất bại')
-          }
-        })
-    }
-   }else{
-     this.updatedata()
-   }
-  }
-  updatedata(){
-    this.api.putdanhsachbenhnhanApi(this.thembenhnhanform.value , this.editdata.id).subscribe({
-      next:(res)=>{
-        alert("Update Succesfully")
-        this.thembenhnhanform.reset()
-        this.dialogRef.close('update')
+            alert('Thêm thất bại');
+          },
+        });
       }
-    })
+    } else {
+      this.updatedata();
+    }
+  }
+  onSubmit(){
+    this.thembenhnhanform.invalid
+  }
+  updatedata() {
+    this.api
+      .putdanhsachbenhnhanApi(this.thembenhnhanform.value, this.editdata.id)
+      .subscribe({
+        next: (res) => {
+          alert('Update Succesfully');
+          this.thembenhnhanform.reset();
+          this.dialogRef.close('update');
+        },
+      });
   }
 }
